f5cee2e4f28b79d1d12334c898588e8ff05f1f9e
Martin Sosic <sosic.martin@gmail.com>
2017-02-18 20:46:24 +0100

    Updated docs

---
+++ menudata.js
@@ -0,0 +1,16 @@
+var menudata={children:[
+{text:'Main Page',url:'index.html'},
+{text:'Classes',url:'annotated.html',children:[
+{text:'Class List',url:'annotated.html'},
+{text:'Class Index',url:'classes.html'},
+{text:'Class Members',url:'functions.html',children:[
+{text:'All',url:'functions.html'},
+{text:'Variables',url:'functions_vars.html'}]}]},
+{text:'Files',url:'files.html',children:[
+{text:'File List',url:'files.html'},
+{text:'File Members',url:'globals.html',children:[
+{text:'All',url:'globals.html'},
+{text:'Functions',url:'globals_func.html'},
+{text:'Enumerations',url:'globals_enum.html'},
+{text:'Enumerator',url:'globals_eval.html'},
+{text:'Macros',url:'globals_defs.html'}]}]}]}
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['edlibalign',['edlibAlign',['../edlib_8h.html#a56ba75228f1286ad1d9be17e3d71abff',1,'edlib.h']]],
-  ['edlibalignmenttocigar',['edlibAlignmentToCigar',['../edlib_8h.html#ae79582ccafacd9d6af2f724904ff3904',1,'edlib.h']]],
-  ['edlibdefaultalignconfig',['edlibDefaultAlignConfig',['../edlib_8h.html#aae9b606bd4fb126967120ea2a74eed19',1,'edlib.h']]],
+  ['edlibalign',['edlibAlign',['../edlib_8h.html#ae263bc878b6837c5ec36a3b25dc854d6',1,'edlib.h']]],
+  ['edlibalignmenttocigar',['edlibAlignmentToCigar',['../edlib_8h.html#a84310235e0d36e19fda12351269e888c',1,'edlib.h']]],
+  ['edlibdefaultalignconfig',['edlibDefaultAlignConfig',['../edlib_8h.html#a046b587c7774537ea7fb9bdce72a30b3',1,'edlib.h']]],
   ['edlibfreealignresult',['edlibFreeAlignResult',['../edlib_8h.html#a5c40e385ff6bd8a72b70840a2780e3a9',1,'edlib.h']]],
   ['edlibnewalignconfig',['edlibNewAlignConfig',['../edlib_8h.html#a72f08f1e82de2fce260bb50c84357088',1,'edlib.h']]]
 ];
