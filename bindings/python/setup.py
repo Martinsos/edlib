bbb8ca1863ade14ca2fab284cbe54a0fdf6bced2
Martin Sosic <sosic.martin@gmail.com>
2018-02-03 16:35:18 +0100

    Fixed version number of python binding.

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -27,7 +27,7 @@ setup(
     name = "edlib",
     description = "Lightweight, super fast library for sequence alignment using edit (Levenshtein) distance.",
     long_description = long_description,
-    version = "1.2.2",
+    version = "1.2.3",
     url = "https://github.com/Martinsos/edlib",
     author = "Martin Sosic",
     author_email = "sosic.martin@gmail.com",
