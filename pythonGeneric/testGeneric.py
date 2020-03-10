355668d6e4632bcb314e7bc4badb12310290bb3b
Mobin <mobin.asri2011@gmail.com>
2020-03-09 22:38:46 -0700

    make long lists (longer than 256)

--- pythonGeneric/testGeneric.py
+++ pythonGeneric/testGeneric.py
@@ -1,7 +1,10 @@
 import edlib
 
-query = [1,2,3,4,5,5,'c']
-target = [1,2,3,4,4,'b']
+#query = [1,2,3,4,5,5,'c']
+#target = [1,2,3,4,4,'b']
+
+query = [i for i in range(500)]
+target = [i for i in range(200, 600)]
 
 result = edlib.align(query, target, task ='path',additionalEqualities=[('c',10),(10,'b')])
 alignment = edlib.getNiceAlignment(result, query, target)
